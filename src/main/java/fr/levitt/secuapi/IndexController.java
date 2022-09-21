48aae46633f0a0c170d516b56c4b2017d3531fd5
Snowky92 <amaurypinheiro@hotmail.fr>
2022-09-21 11:53:11 +0200

    Update IndexController.java

--- src/main/java/fr/levitt/secuapi/IndexController.java
+++ src/main/java/fr/levitt/secuapi/IndexController.java
@@ -1,17 +1,13 @@
 package fr.levitt.secuapi;
 
-import fr.levitt.secuapi.core.Utilisateur;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public Utilisateur index() {
-        Utilisateur utilisateur = new Utilisateur();
-        utilisateur.setNom("Toto");
-        utilisateur.setPrenom("Tata");
-        return utilisateur;
-    }
+	@GetMapping("/")
+	public void index() {
+
+	}
 }
